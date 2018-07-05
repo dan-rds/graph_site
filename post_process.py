cdd557e8e83bfd366dc39fcda52edcbb5172df30
daniel <ddrichar@ucc.edu>
2018-07-05 13:34:56 -0700

    push to google vm

--- post_process.py
+++ post_process.py
@@ -22,7 +22,7 @@ for parent_dir in dirs:
 	head = {}
 	print (dir_dict)
 	reports = os.listdir('./glance/tree_data/' + parent_dir)
-	print (reports)
+	print ("REPORTS : ", reports)
 	for f in reports:
 
 		if 'h' in f:
@@ -36,8 +36,9 @@ for parent_dir in dirs:
 			heads_children.append(json.loads(json_file))
 			print( parent_dir + "/" + f)
 		
-	head["children"] =  sorted(heads_children, key=itemgetter('hostname'))
+	head["children"] =  sorted(heads_children, key=itemgetter('Hostname'))
 	#print("append", len(heads_children), len(head["children"]),"\n\n\n\n\n\n")
+	dir_dict["Name"] = parent_dir
 	dir_dict["children"].append(head)
 
 	root["children"].append(dir_dict)
@@ -53,7 +54,7 @@ dirs = os.listdir('./glance/csv_data/')
 
 
 import pandas as pd
-types = ["NICs", "GPUs", "CPUs", "Disks", "Memory", "SYS"]
+types = ["NICs", "GPUs", "CPUs", "Disks", "Memory", "System"]
 for t in types:
 	combined_csv = pd.concat( [ pd.read_csv('./glance/csv_data/' + f ) for f in list(filter(lambda x: t in x, dirs)) ] )
 	combined_csv.to_csv( t + ".csv", index=False )
